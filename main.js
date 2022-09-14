a0f621f73ce9c01d7798aba5270f45bcb4e7d125
Hikari7 <h.kobe712@gmail.com>
2022-09-13 21:42:15 -0700

    fixed some layouts and JS

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const open = document.querySelector(".open");
-const overlay = document.querySelector(".overlay");
 const close = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+const contact = document.querySelector(".nav_item_contact");
+const pricing = document.querySelector(".nav_item_pricing");
+const about = document.querySelector(".nav_item_about");
 
 open.addEventListener("click", () => {
   overlay.classList.add("show");
 });
 
 close.addEventListener("click", () => {
+  overlay.classList.remove("show");
+  console.log("click");
+});
+
+contact.addEventListener("click", () => {
+  overlay.classList.remove("show");
+});
+
+pricing.addEventListener("click", () => {
+  overlay.classList.remove("show");
+});
+
+about.addEventListener("click", () => {
   overlay.classList.remove("show");
 });
