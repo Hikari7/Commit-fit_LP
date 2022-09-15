94695d91f6a74d970c8572caf3bfddc1714a9b33
Hikari7 <h.kobe712@gmail.com>
2022-09-14 22:58:25 -0700

    add JS and animations

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Hamburger menu
+
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
@@ -11,7 +13,6 @@ open.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   overlay.classList.remove("show");
-  console.log("click");
 });
 
 contact.addEventListener("click", () => {
@@ -25,3 +26,34 @@ pricing.addEventListener("click", () => {
 about.addEventListener("click", () => {
   overlay.classList.remove("show");
 });
+
+//appear about
+const upperItems = document.querySelectorAll(".upper_hidden");
+const bottomItems = document.querySelectorAll(".bottom_hidden");
+const cards = document.querySelectorAll(".card");
+
+function showItem() {
+  const scrolled = window.scrollY; //ユーザーがスクロールした分
+  upperItems.forEach((uItem) => {
+    const topItem = uItem.offsetTop;
+    if (scrolled + window.innerHeight > topItem) {
+      uItem.classList.add("appear_upper");
+    }
+  });
+
+  bottomItems.forEach((bItem) => {
+    const topItemDelay = bItem.offsetTop;
+    if (scrolled + window.innerHeight > topItemDelay) {
+      bItem.classList.add("appear_bottom");
+    }
+  });
+
+  cards.forEach((card) => {
+    const topCard = card.offsetTop;
+    if (scrolled + window.innerHeight > topCard) {
+      card.classList.add("appear_card");
+    }
+  });
+}
+
+window.addEventListener("scroll", showItem);
